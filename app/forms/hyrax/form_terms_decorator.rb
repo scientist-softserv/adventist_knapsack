980b8340e429035742b3c3284c7e82696f279354
Shana Moore <shana@scientist.com>
2023-09-29 14:10:14 -0700

    🧹 Add FormTermsDecorator
    This commit will make the base primary terms match adventist proper.
    
    Issue:
    - https://github.com/scientist-softserv/adventist-dl/issues/585
    
    Co-Authored-By: Kirk Wang <k3wang@gmail.com>

--- app/forms/hyrax/form_terms_decorator.rb
+++ app/forms/hyrax/form_terms_decorator.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 module Hyrax
-  module FormTerms
-    include Hyrax::Forms
+  module FormTermsDecorator
+
+    # include Hyrax::Forms
     # overrides Hyrax::Forms::WorkForm
     # to display 'license' in the 'base-terms' div on the user dashboard "Add New Work" description
     # by getting iterated over in hyrax/app/views/hyrax/base/_form_metadata.html.erb
     def primary_terms
-      super + %i[creator keyword license]
+      super + %i[creator keyword]
     end
   end
 end
+
+Hyrax::FormTerms.prepend(Hyrax::FormTermsDecorator)
