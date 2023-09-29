069f4e50621e25cd3aca9018c859781e1e64112a
LaRita Robinson <larita@scientist.com>
2023-09-29 14:33:57 -0400

    🧹 Fix appearance fonts and colors
    - Review appearance styles to verify which colors are being used.
    - Set up appropriate custom colors in appearance decorator, based on
    what is actually used in appearance_styles.
    - bring in default_colors_form to be able to render the color list.

--- app/forms/hyrax/forms/admin/appearance_decorator.rb
+++ app/forms/hyrax/forms/admin/appearance_decorator.rb
@@ -1,35 +1,68 @@
 # frozen_string_literal: true
 
+# OVERRIDE to add adventist's custom colors and fonts
 module Hyrax
   module Forms
     module Admin
       module AppearanceDecorator
-        DEFAULT_COLORS = {
-          'header_and_footer_background_color' => '#3c3c3c',
-          'header_and_footer_text_color'       => '#dcdcdc',
-          'navbar_background_color'            => '#000000',
-          'navbar_link_background_hover_color' => '#ffffff',
-          'navbar_link_text_color'             => '#eeeeee',
-          'navbar_link_text_hover_color'       => '#eeeeee',
-          'link_color'                         => '#2e74b2',
-          'link_hover_color'                   => '#215480',
-          'footer_link_color'                  => '#ffebcd',
-          'footer_link_hover_color'            => '#ffffff',
-          'primary_button_hover_color'         => '#286090',
-          'default_button_background_color'    => '#ffffff',
-          'default_button_border_color'        => '#cccccc',
-          'default_button_text_color'          => '#333333',
-          # 'active_tabs_background_color'     => '#337ab7',
-          'facet_panel_background_color'       => '#f5f5f5',
-          'facet_panel_text_color'             => '#333333',
-          'custom_adl_header_footer_color'     => '#CE8C00'
+        ADL_DEFAULT_COLORS = {
+          'custom_adl_header_footer_color'        => '#CE8C00',
+          'header_background_color'               => '#000000',
+          'active_tabs_background_color'          => '#3c3c3c',
+          'header_background_border_color'       => '#000000',
+          'header_text_color'                     => '#2C2C2C',
+          # 'navbar_background_color'             => '#ffffff',
+          # 'navbar_link_background_hover_color'  => '#ffffff',
+          # 'navbar_link_text_color'              => '#eeeeee',
+          # 'navbar_link_text_hover_color'        => '#eeeeee',
+          'link_color'                            => '#2e74b2',
+          'link_hover_color'                      => '#215480',
+          'footer_link_color'                     => '#ffebcd',
+          'footer_link_hover_color'               => '#ffffff',
+          'primary_button_background_color'       => '#CE8C00',
+          'primary_button_border_color'           => '#CE8C00',
+          'primary_button_focus_background_color' => '#CE8C00',
+          'primary_button_focus_border_color'     => '#CE8C00',
+          'primary_button_hover_background_color' => '#286090',
+          'primary_button_hover_border_color'     => '#286090',
+          'default_button_background_color'       => '#ffffff',
+          'default_button_border_color'           => '#cccccc',
+          'default_button_text_color'             => '#333333',
+          'default_button_focus_background_color' => '#333333',
+          'default_button_focus_border_color'     => '#333333',
+          'default_button_hover_background_color' => '#333333',
+          'default_button_hover_border_color'     => '#333333',
+          'default_button_text_color'             => '#333333',
+          'active_tabs_background_color'          => '#337ab7',
+          'facet_panel_background_color'          => '#f5f5f5',
+          'facet_panel_text_color'                => '#333333',
+          'facet_panel_border_color'              => '#f5f5f5'
         }.freeze
 
-        # TODO: do we need this adl_header_footer_color?
-        # 'custom_adl_header_footer_color'   => '#CE8C00'
-        # OVERRIDE here to add adventist's custom header & footer
+        ADL_DEFAULT_FONTS = {
+          'body_font'     => 'Nobile, sans-serif;',
+          'headline_font' => 'Nobile, sans-serif;'
+        }.freeze
+
+        # OVERRIDE to add adventist's custom header & footer
+        def default_values
+          ADL_DEFAULT_FONTS.merge(ADL_DEFAULT_COLORS)
+        end
+
         def custom_adl_header_footer_color
           block_for('custom_adl_header_footer_color')
+        end
+
+        def header_background_color
+          block_for('header_background_color')
+        end
+
+        def header_text_color
+          block_for('header_text_color')
+        end
+
+        def primary_button_background_color
+          block_for('primary_button_background_color')
         end
       end
     end
