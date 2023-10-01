356a4c0b33a6382828e5ab9a15a0cc0f3cd38e13
Rob Kaufman <rob@notch8.com>
2023-09-30 23:11:38 -0400

    errors with deployed code

---
+++ app/actors/hyrax/actors/create_with_remote_files_actor_decorator.rb
@@ -0,0 +1,46 @@
+# OVERRIDE Hyrax 3.6 to add custom default thumbnails
+
+module Hyrax
+  module Actors
+    module CreateWithRemoteFilesActorDecorator
+      module IngestRemoteFilesServiceDecorator
+        ##
+        # @return true
+        def attach!
+          return true unless remote_files
+          remote_files.each do |file_info|
+            next if file_info.blank? || file_info[:url].blank?
+            # Escape any space characters, so that this is a legal URI
+            uri = URI.parse(Addressable::URI.escape(file_info[:url]))
+            unless self.class.validate_remote_url(uri)
+              Rails.logger.error "User #{user.user_key} attempted to ingest file from url #{file_info[:url]}, which doesn't pass validation"
+              return false
+            end
+            auth_header = file_info.fetch(:auth_header, {})
+            # OVERRIDE Hyrax 3.5 to override default_thumbnail
+            create_file_from_url(uri, file_info[:file_name], auth_header, file_info[:default_thumbnail])
+          end
+          add_ordered_members! if ordered
+          true
+        end
+
+        def create_file_from_url(uri, file_name, auth_header, default_thumbnail = nil)
+          import_url = URI.decode_www_form_component(uri.to_s)
+          use_valkyrie = false
+          case curation_concern
+          when Valkyrie::Resource
+            file_set = Hyrax.persister.save(resource: Hyrax::FileSet.new(import_url: import_url, label: file_name))
+            use_valkyrie = true
+          else
+            # OVERRIDE Hyrax 3.5 to override default_thumbnail
+            file_set = ::FileSet.new(import_url: import_url, label: file_name, override_default_thumbnail: override_default_thumbnail)
+          end
+          __create_file_from_url(file_set: file_set, uri: uri, auth_header: auth_header, use_valkyrie: use_valkyrie)
+        end
+
+      end
+    end
+  end
+end
+
+Hyrax::Actors::CreateWithRemoteFilesActor::IngestRemoteFilesService.prepend Hyrax::Actors::CreateWithRemoteFilesActorDecorator::IngestRemoteFilesServiceDecorator
