e72692ce76da0dd1c684bb59d8f2939e5f9e30a5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-10-09 16:49:08 -0400

    🧹 Ensure delegate behaves as desired
    In reviewing files that are changed in Knapsack versus what's in
    Hyku, this was a case where the prepends were not behaving as desired.

--- app/presenters/hyrax/collection_presenter_decorator.rb
+++ app/presenters/hyrax/collection_presenter_decorator.rb
@@ -4,8 +4,6 @@
 module Hyrax
   module CollectionPresenterDecorator
     module ClassMethods
-      delegate(*Collection.additional_terms, to: :solr_document)
-
       # Terms is the list of fields displayed by
       # app/views/collections/_show_descriptions.html.erb
       def terms
@@ -21,8 +19,6 @@ module Hyrax
   end
 end
 
-# Why singleton_class?  I tried to use ActiveSupport::Concern.included and .class_methods but this
-# just didn't work.  Instead I'm leveraging the old-school Rails idiom of having a ClassMethods
-# module; and prepending that to the singleton_class (e.g. make the methods of the ClassMethods
-# module be class methods on the Hyrax::CollectionPresenter)
-Hyrax::CollectionPresenter.singleton_class.send(:prepend, Hyrax::CollectionPresenterDecorator)
+Hyrax::CollectionPresenter.delegate(*Collection.additional_terms, to: :solr_document)
+Hyrax::CollectionPresenter.prepend(Hyrax::CollectionPresenterDecorator)
+Hyrax::CollectionPresenter.singleton_class.prepend(Hyrax::CollectionPresenterDecorator::ClassMethods)
