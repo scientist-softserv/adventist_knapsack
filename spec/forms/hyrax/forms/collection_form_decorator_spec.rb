c165a8f66c2083a20f0eef462f6ba91a1a7092d6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-10-12 16:27:58 -0400

    💄 Appease rubocop

--- spec/forms/hyrax/forms/collection_form_decorator_spec.rb
+++ spec/forms/hyrax/forms/collection_form_decorator_spec.rb
@@ -7,51 +7,53 @@ RSpec.describe Hyrax::Forms::CollectionForm do
     subject { described_class.terms.sort }
 
     # NOTE: There shall not be alternative_title that conflicts with slugs.
+    # rubocop:disable RSpec/ExampleLength
     it do
       is_expected.to match_array(
-                       %i[abstract
-                          alt
-                          based_near
-                          collection_type_gid
-                          contributor
-                          creator
-                          date
-                          date_accepted
-                          date_available
-                          date_created
-                          date_issued
-                          date_published
-                          date_submitted
-                          department
-                          description
-                          doi
-                          former_identifier
-                          funder
-                          identifier
-                          issue_number
-                          keyword
-                          language
-                          lat
-                          license
-                          location
-                          long
-                          managing_organisation
-                          note
-                          official_url
-                          output_of
-                          pagination
-                          part_of
-                          place_of_publication
-                          publication_status
-                          publisher
-                          related_url
-                          representative_id
-                          resource_type
-                          subject
-                          thumbnail_id
-                          title
-                          visibility]
-                     )
+        %i[abstract
+           alt
+           based_near
+           collection_type_gid
+           contributor
+           creator
+           date
+           date_accepted
+           date_available
+           date_created
+           date_issued
+           date_published
+           date_submitted
+           department
+           description
+           doi
+           former_identifier
+           funder
+           identifier
+           issue_number
+           keyword
+           language
+           lat
+           license
+           location
+           long
+           managing_organisation
+           note
+           official_url
+           output_of
+           pagination
+           part_of
+           place_of_publication
+           publication_status
+           publisher
+           related_url
+           representative_id
+           resource_type
+           subject
+           thumbnail_id
+           title
+           visibility]
+      )
     end
+    # rubocop:enable RSpec/ExampleLength
   end
 end
