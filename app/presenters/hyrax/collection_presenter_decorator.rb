37461bcc8ceaeee36761b8f3c12c80cb4963ddb1
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-10-12 15:07:47 -0400

    🐛 Return identifier to collection show
    The identifier was removed from collections via [PR 16][1].  And in #620
    we have been asked to restore that.
    
    I have looked and the collection does not have an Alternate Title field,
    so I don't know how to address/resolve the request for that property to
    show.
    
    There are complications with the alternative (as envisioned in Hyrax)
    because we have overloaded the meaning of that predicate to be the slug.
    This will create data issues if we choose to re-introduce alternative
    title.
    
    Related to:
    
    - https://github.com/scientist-softserv/adventist-dl/issues/620
    - https://github.com/scientist-softserv/adventist-dl/issues/578
    
    [1]: https://github.com/scientist-softserv/adventist_knapsack/pull/16

--- app/presenters/hyrax/collection_presenter_decorator.rb
+++ app/presenters/hyrax/collection_presenter_decorator.rb
@@ -10,15 +10,8 @@ module Hyrax
         super + Collection.additional_terms
       end
     end
-
-    def terms_with_values
-      # OVERRIDE HYRAX 3.5.0 to hide identifier line
-      terms = self.class.terms - [:identifier]
-      terms.select { |t| self[t].present? }
-    end
   end
 end
 
 Hyrax::CollectionPresenter.delegate(*Collection.additional_terms, to: :solr_document)
-Hyrax::CollectionPresenter.prepend(Hyrax::CollectionPresenterDecorator)
 Hyrax::CollectionPresenter.singleton_class.prepend(Hyrax::CollectionPresenterDecorator::ClassMethods)
