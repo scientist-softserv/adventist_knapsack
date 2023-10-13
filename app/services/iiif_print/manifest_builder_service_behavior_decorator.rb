f5ace1a1d94ad9653808db868d9cdb475319d410
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-10-13 13:42:47 -0400

    ♻️ Move decoration to after initialize
    This is a guess as to some of the loading issues.  Namely that I have
    specs that say the code for a class should be in "one file" but is in a
    "different one".  That indicates the "different one" is being loaded
    after the "one file".
    
    So, I'm looking to see if I can adjust the load sequence of the
    decorators.
    
    Update, based on one run with these specs in the test suite, things
    worked.  Hopefully this alleviates further problems.

--- app/services/iiif_print/manifest_builder_service_behavior_decorator.rb
+++ app/services/iiif_print/manifest_builder_service_behavior_decorator.rb
@@ -1,44 +1,46 @@
 # frozen_string_literal: true
 
-module HykuKnapsack::ManifestBuilderServiceDecorator
-  def build_manifest(presenter:)
-    returning_hash = super
-    returning_hash['rendering'] = rendering(presenter: presenter)
-    returning_hash
-  end
-
-  def rendering(presenter:)
-    file_set_presenters = presenter.file_set_presenters.reject { |fsp| fsp.mime_type.include?('image') }
-
-    file_set_presenters.map do |fsp|
-      {
-        # Yes, we are using `#send` because `#hostname` is a private method, though I think it's okay here
-        "@id": Hyrax::Engine.routes.url_helpers.download_url(fsp.id, host: presenter.send(:hostname)),
-        "label": fsp.label,
-        "format": fsp.mime_type
-      }
+module HykuKnapsack
+  module ManifestBuilderServiceDecorator
+    def build_manifest(presenter:)
+      returning_hash = super
+      returning_hash['rendering'] = rendering(presenter: presenter)
+      returning_hash
     end
-  end
 
-  def sanitize_v2(hash:, presenter:, solr_doc_hits:)
-    hash['label'] = sanitize_label(hash['label']) if hash.key?('label')
-    hash.delete('description') # removes default description since it's in the metadata fields
-    hash['sequences']&.each do |sequence|
-      # removes canvases if there are thumbnail files
-      sequence['canvases'].reject! do |canvas|
-        sanitize_label(canvas['label']).end_with?('.TN.jpg')
-      end
+    def rendering(presenter:)
+      file_set_presenters = presenter.file_set_presenters.reject { |fsp| fsp.mime_type.include?('image') }
 
-      sequence['canvases']&.each do |canvas|
-        canvas['label'] = sanitize_label(canvas['label'])
-        apply_metadata_to_canvas(canvas: canvas, presenter: presenter, solr_doc_hits: solr_doc_hits)
+      file_set_presenters.map do |fsp|
+        {
+          # Yes, we are using `#send` because `#hostname` is a private method, though I think it's okay here
+          "@id": Hyrax::Engine.routes.url_helpers.download_url(fsp.id, host: presenter.send(:hostname)),
+          "label": fsp.label,
+          "format": fsp.mime_type
+        }
       end
     end
-    hash
-  end
 
-  def sanitize_label(label)
-    CGI.unescapeHTML(sanitize_value(label))
+    def sanitize_v2(hash:, presenter:, solr_doc_hits:)
+      hash['label'] = sanitize_label(hash['label']) if hash.key?('label')
+      hash.delete('description') # removes default description since it's in the metadata fields
+      hash['sequences']&.each do |sequence|
+        # removes canvases if there are thumbnail files
+        sequence['canvases'].reject! do |canvas|
+          sanitize_label(canvas['label']).end_with?('.TN.jpg')
+        end
+
+        sequence['canvases']&.each do |canvas|
+          canvas['label'] = sanitize_label(canvas['label'])
+          apply_metadata_to_canvas(canvas: canvas, presenter: presenter, solr_doc_hits: solr_doc_hits)
+        end
+      end
+      hash
+    end
+
+    def sanitize_label(label)
+      CGI.unescapeHTML(sanitize_value(label))
+    end
   end
 end
 # rubocop:enable Metrics/BlockLength
