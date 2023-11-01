091aac5fd544d4e66da0ff509fe5a008ebc5fb06
Kirk Wang <kirk.wang@scientist.com>
2023-11-01 11:39:42 -0700

    🐛 Fix UV download option for PDFs
    This commit will adjust the #rendering for maninfests to render a PDF
    download option for UV.  After the upgrade, IiifManifestPresenters don't
    seem to have the file_set_presenters we're looking for anymore.  We
    instead instantiate a Hyrax presenter which does have the needed
    file_set_presenters.
    
    Ref:
      - https://github.com/scientist-softserv/adventist-dl/issues/637

--- app/services/iiif_print/manifest_builder_service_behavior_decorator.rb
+++ app/services/iiif_print/manifest_builder_service_behavior_decorator.rb
@@ -8,19 +8,6 @@ module HykuKnapsack
       returning_hash
     end
 
-    def rendering(presenter:)
-      file_set_presenters = presenter.file_set_presenters.reject { |fsp| fsp.mime_type.include?('image') }
-
-      file_set_presenters.map do |fsp|
-        {
-          # Yes, we are using `#send` because `#hostname` is a private method, though I think it's okay here
-          "@id": Hyrax::Engine.routes.url_helpers.download_url(fsp.id, host: presenter.send(:hostname)),
-          "label": fsp.label,
-          "format": fsp.mime_type
-        }
-      end
-    end
-
     def sanitize_v2(hash:, presenter:, solr_doc_hits:)
       hash['label'] = sanitize_label(hash['label']) if hash.key?('label')
       hash.delete('description') # removes default description since it's in the metadata fields
@@ -38,11 +25,31 @@ module HykuKnapsack
       hash
     end
 
-    def sanitize_label(label)
-      CGI.unescapeHTML(sanitize_value(label))
-    end
+    private
+
+      def rendering(presenter:)
+        model = presenter.solr_document['has_model_ssim'].first
+        # Our current presenter is a IiifManifestPresenter, which doesn't have the file_set_presenters we need.
+        # So we create a Hyrax presenter for the model, and use that to get the file_set_presenters.
+        hyrax_presenter = "Hyrax::#{model}Presenter".constantize.new(presenter, presenter.ability)
+        file_set_presenters = hyrax_presenter.file_set_presenters.reject { |fsp| fsp.mime_type.include?('image') }
+
+        file_set_presenters.map do |fsp|
+          {
+            # Yes, we are using `#send` because `#hostname` is a private method, though I think it's okay here
+            "@id": Hyrax::Engine.routes.url_helpers.download_url(fsp.id,
+                                                                 host: presenter.send(:hostname),
+                                                                 protocol: 'https'),
+            "label": fsp.label,
+            "format": fsp.mime_type
+          }
+        end
+      end
+
+      def sanitize_label(label)
+        CGI.unescapeHTML(sanitize_value(label))
+      end
   end
 end
-# rubocop:enable Metrics/BlockLength
 
 Hyrax::ManifestBuilderService.prepend(HykuKnapsack::ManifestBuilderServiceDecorator)
