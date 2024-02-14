ebaab989de8c579af25a353f0e09156b3da7c6ec
Shana Moore <shana@scientist.com>
2024-02-14 13:18:18 -0800

    Revert setting HYKU_GEONAMES_USERNAME
    Each tenant must set their own geonames username in their account settings. Setting the env var does nothing onces deployed to the various environments.

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -16,9 +16,4 @@ Hyrax.config do |config|
   # Also, we will continue to extract txt file's text using Adventist::TextFileTextExtractionService
   config.extract_full_text = false
   config.work_requires_files = false
-
-  # Location autocomplete uses geonames to search for named regions.
-  # Specify the user for connecting to geonames:
-  # Register here: http://www.geonames.org/manageaccount
-  config.geonames_username = ENV.fetch('HYKU_GEONAMES_USERNAME', 'jcoyne')
 end
