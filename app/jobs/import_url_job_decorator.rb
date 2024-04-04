a6afdd8befae5d9c582223f015277a688255c752
Shana Moore <shana@scientist.com>
2024-04-04 15:30:48 -0700

    git diff 032f58450daf90a6db8e88a1279979fe524a3e0d
    pull over code that was introduced in adventist-dl post knapsack
    
    Issue:
    - https://github.com/scientist-softserv/adventist-dl/issues/741

--- app/jobs/import_url_job_decorator.rb
+++ app/jobs/import_url_job_decorator.rb
@@ -1,33 +1,39 @@
 # frozen_string_literal: true
 
-# OVERRIDE in v3.5.0 of Hyrax
+# OVERRIDE in v2.9.6 of Hyrax
 module ImportUrlJobDecorator
   # OVERRIDE to gain further insight into the StandardError that was reported but hidden.
-  def copy_remote_file(name)
+  def copy_remote_file(uri, name, headers = {})
     filename = File.basename(name)
     dir = Dir.mktmpdir
     Rails.logger.debug("ImportUrlJob: Copying <#{uri}> to #{dir}")
 
-    begin
-      File.open(File.join(dir, filename), 'wb') do |f|
-        write_file(f)
+    File.open(File.join(dir, filename), 'wb') do |f|
+      begin
+        write_file(uri, f, headers)
         yield f
+      rescue StandardError => e
+        # OVERRIDE adding Rails.logger.error call
+        Rails.logger.error(
+          %(ImportUrlJob: Error copying <#{uri}> to #{dir} with #{e.message}.  #{e.backtrace.join("\n")})
+        )
+        send_error(e.message + e.backtrace.join("\n"))
+        # TODO: Should we re-raise the exception?  As written this copy_remote_file has a false
+        # success.
       end
-    rescue StandardError => e
-      Rails.logger.error(
-        %(ImportUrlJob: Error copying <#{uri}> to #{dir} with #{e.message}.  #{e.backtrace.join("\n")})
-      )
-      send_error(e.message)
-      # TODO: Consider re-raising the exception if needed
     end
-    Rails.logger.debug("ImportUrlJob: Closing #{File.join(dir, filename)}")
+    Rails.logger.debug("ImportUrlJob: Copying <#{uri}> to #{dir}, closing #{File.join(dir, filename)}")
   end
 
   # OVERRIDE there are calls to send_error that send two arguments.
+  # also give us the fileset with the error
   #
   # @see https://github.com/samvera/hyrax/blob/426575a9065a5dd3b30f458f5589a0a705ad7be2/app/jobs/import_url_job.rb#L76-L105
   def send_error(error_message, *)
-    super(error_message)
+    user = User.find_by_user_key(file_set.depositor)
+    @file_set.errors.add("Error for file_set #{@file_set.id}:", error_message)
+    Hyrax.config.callback.run(:after_import_url_failure, @file_set, user)
+    @operation.fail!(@file_set.errors.full_messages.join(' '))
   end
 end
 
