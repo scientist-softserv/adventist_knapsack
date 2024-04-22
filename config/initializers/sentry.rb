35eb3a0931b54bd4f9782709dc9fd0a7b6a76cbf
Rob Kaufman <rob@notch8.com>
2024-04-21 23:40:39 -0700

    do not report 404s

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,5 +5,7 @@ Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger, :sentry_logger]
   config.enabled_environments = %w[adv-knapsack-staging adv-knapsack-production]
+  # Skip Hyrax 404s
+  config.excluded_exceptions += ['Hyrax::ObjectNotFoundError']
   config.debug = false
 end
