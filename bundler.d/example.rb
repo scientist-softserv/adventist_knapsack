c7ae893733a19c911ecf7030d4ceac38fc74419d
Shana Moore <shana@scientist.com>
2024-07-18 10:35:10 -0700

    Update submodule

---
+++ bundler.d/example.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+# see https://github.com/kbrock/bundler-inject/tree/gem_path
+
+# specify one or more ruby files in this directory to be injected into bundler
+# you can use `gem` to add new gems, `override_gem` to change an existing gem
+# or `ensure_gem` to make sure a gem is there w/o worrying about if it is an
+# override or not
+
+gem "sentry-ruby"
+gem "sentry-rails"
+gem "sentry-sidekiq"
