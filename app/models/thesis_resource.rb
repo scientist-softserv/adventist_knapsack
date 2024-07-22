22474c80549cee0712da551637d3e00cfdae229f
LaRita Robinson <larita@scientist.com>
2024-07-22 13:59:42 -0400

    Update thesis resource from generated

--- app/models/thesis_resource.rb
+++ app/models/thesis_resource.rb
@@ -5,4 +5,21 @@
 class ThesisResource < Hyrax::Work
   include Hyrax::Schema(:basic_metadata)
   include Hyrax::Schema(:thesis_resource)
+  include Hyrax::Schema(:bulkrax_metadata)
+  include Hyrax::Schema(:with_video_embed)
+
+  include Hyrax::ArResource
+  include Hyrax::NestedWorks
+
+  Hyrax::ValkyrieLazyMigration.migrating(self, from: Thesis)
+
+  include IiifPrint.model_configuration(
+    pdf_split_child_model: ThesisResource,
+    pdf_splitter_service: IiifPrint::SplitPdfs::AdventistPagesToJpgsSplitter,
+    derivative_service_plugins: [
+      IiifPrint::TextExtractionDerivativeService
+    ]
+  )
+
+  prepend OrderAlready.for(:creator)
 end
