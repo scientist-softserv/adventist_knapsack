9754fa86c49dc9d1cf43677afc0c8cde49a194d3
LaRita Robinson <larita@scientist.com>
2024-07-30 15:08:05 -0400

    Use Hyrax::SolrService to remove extra solr index

--- lib/adl/transactions/steps/set_slug.rb
+++ lib/adl/transactions/steps/set_slug.rb
@@ -12,17 +12,16 @@ module Adl
           change_set.slug = change_set.set_slug
 
           # if we add/change a slug with an existing record, previous indexes would have a different id,
-          # resulting extraneous solr indexes remaining (one fedora object/resource with two solr indexes
+          # resulting extraneous solr indexes remaining (one fedora object/resource with multiple solr indexes
           # to it)
           #   1) This happens when a slug gets changed from either empty or a different value
           #   2) It also apparently happens in some situations where data existed prior to the slug logic
-          # This query finds everything indexed by fedora id. The new index will have id: slug
-          #
-          # When the slug changed or we are dealing the ActiveFedora::Persistence.delete we need to both
-          # the solr doc id (which was/is the slug) and the fedora_id_ssi or resource_id_ssi (which is the
-          # object's ID)
-          Blacklight.default_index.connection.delete_by_query('id:"' + change_set.id.to_s + '" OR resource_id_ssi:"' + change_set.id.to_s + '" OR fedora_id_ssi:"' + change_set.id.to_s + '"')
-          Blacklight.default_index.connection.commit
+          # This query finds everything indexed by the original object's id. The new index will have id: slug.
+          original_id = change_set.id.to_s
+          unless original_id.blank?
+            Hyrax::SolrService.delete_by_query('id:"' + change_set.id.to_s + '" OR resource_id_ssi:"' + change_set.id.to_s + '" OR fedora_id_ssi:"' + change_set.id.to_s + '"')
+            Hyrax::SolrService.commit
+          end
 
           Success(change_set)
         rescue NoMethodError => err
