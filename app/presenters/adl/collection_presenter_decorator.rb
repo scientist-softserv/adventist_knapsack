68a5470a76bc312c30b55111e827492458704065
Kirk Wang <kirk.wang@scientist.com>
2024-08-14 11:27:35 -0700

    ⚙️ Move Engine's to_prepare to an initializer
    This commit will resolve load ordering.  Previously, we were loading the
    decorators from HykuKnapsack before we load the App's, which is standard
    behavior, but we actually want the decorators from HykuKnapsack to load
    last.

--- app/presenters/adl/collection_presenter_decorator.rb
+++ app/presenters/adl/collection_presenter_decorator.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 # OVERRIDE HYRAX 3.5.0
-module Hyrax
+module Adl
   module CollectionPresenterDecorator
     module ClassMethods
       # Terms is the list of fields displayed by
       # app/views/collections/_show_descriptions.html.erb
       def terms
-        super + Collection.additional_terms
+        super - [:size] + Collection.additional_terms
       end
     end
   end
 end
 
-Hyrax::CollectionPresenter.singleton_class.prepend(Hyrax::CollectionPresenterDecorator::ClassMethods)
+Hyrax::CollectionPresenter.singleton_class.prepend(Adl::CollectionPresenterDecorator::ClassMethods)
