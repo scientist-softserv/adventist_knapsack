7e3154be84cd525658fb77113e39f79faf1aca84
Kirk Wang <kirk.wang@scientist.com>
2024-08-21 11:24:09 -0700

    🐛 Wrap slug_valkyrie_overrides in to_prepare
    Originally we had a require 'frigg' in this initializer because we want
    frigg to log and then load the slug logic.  However, this threw off the
    load order of things and caused the Hyrax::PcdmCollection to be
    registered for Collection instead of CollectionResource.  Wrapping this
    in a to_prepare will ensure it's loaded at the right time and keep it
    loaded in dev.

--- config/initializers/slug_valkyrie_overrides.rb
+++ config/initializers/slug_valkyrie_overrides.rb
@@ -1,28 +1,28 @@
 # frozen_string_literal: true
 
-require 'frigg'
+Rails.application.config.to_prepare do
+  # override lib/valkyrie/persistence/postgres/query_service.rb
+  Valkyrie::Persistence::Postgres::QueryService.class_eval do
+    def find_by(id:)
+      result = self.custom_queries.find_by_slug(slug: id)
+      return result if result
 
-# override lib/valkyrie/persistence/postgres/query_service.rb
-Valkyrie::Persistence::Postgres::QueryService.class_eval do
-  def find_by(id:)
-    result = self.custom_queries.find_by_slug(slug: id)
-    return result if result
-
-    # If not found, try by id
-    id = Valkyrie::ID.new(id.to_s) if id.is_a?(String)
-    validate_id(id)
-    resource_factory.to_resource(object: orm_class.find(id.to_s))
-  rescue ActiveRecord::RecordNotFound
-    nil
+      # If not found, try by id
+      id = Valkyrie::ID.new(id.to_s) if id.is_a?(String)
+      validate_id(id)
+      resource_factory.to_resource(object: orm_class.find(id.to_s))
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
   end
-end
 
-# override lib/wings/valkyrie/query_service.rb
-#          This adds in find_by_slug, which uses the overridden ActiveFedora find
-#          so it should always find without going to alternate_identifier
-Wings::Valkyrie::QueryService.class_eval do
-  def find_by(id:)
-    self.custom_queries.find_by_slug(slug: id) || 
-    self.find_by_alternate_identifier(alternate_identifier: id)
+  # override lib/wings/valkyrie/query_service.rb
+  #          This adds in find_by_slug, which uses the overridden ActiveFedora find
+  #          so it should always find without going to alternate_identifier
+  Wings::Valkyrie::QueryService.class_eval do
+    def find_by(id:)
+      self.custom_queries.find_by_slug(slug: id) ||
+      self.find_by_alternate_identifier(alternate_identifier: id)
+    end
   end
 end
