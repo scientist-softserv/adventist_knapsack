1ca87fcac1f39295579d40dc6505cc1ef640d105
LaRita Robinson <larita@scientist.com>
2024-08-27 16:57:22 -0400

    Override IiifPrint config
    GenericWork and Image resource models come from Hyku. They
    need to use custom configuration.

--- app/models/generic_work_resource_decorator.rb
+++ app/models/generic_work_resource_decorator.rb
@@ -2,3 +2,10 @@
 
 GenericWorkResource.include Hyrax::Schema(:slug_metadata)
 GenericWorkResource.include(SlugBugValkyrie)
+GenericWorkResource.prepend(IiifPrint.model_configuration(
+                            pdf_split_child_model: GenericWorkResource,
+                            pdf_splitter_service: IiifPrint::SplitPdfs::AdventistPagesToJpgsSplitter,
+                            derivative_service_plugins: [
+                              IiifPrint::TextExtractionDerivativeService
+                            ]
+                          ))
