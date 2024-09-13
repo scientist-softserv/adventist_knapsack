c1ff8b408f7d61bea7c468bcdb195268ab4d206f
Shana Moore <shana@scientist.com>
2024-09-13 14:28:18 -0700

    lease_release_date => lease_expiration_date

--- config/initializers/iiif_print.rb
+++ config/initializers/iiif_print.rb
@@ -116,7 +116,7 @@ IiifPrint.config do |config|
         visibility: 'lease',
         visibility_after_lease: lease.visibility_after_lease,
         visibility_during_lease: lease.visibility_during_lease,
-        lease_release_date: lease.lease_release_date
+        lease_expiration_date: lease.lease_expiration_date
       }
     else
       visibility_params = { visibility: parent_work.visibility.to_s }
