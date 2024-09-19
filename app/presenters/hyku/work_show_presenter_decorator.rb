3f101b2f7fb720026cd31665e4e0d1ae8dabf471
LaRita Robinson <larita@scientist.com>
2024-09-19 17:24:01 -0400

    Clean up viewer logic
    Overrides logic related to media viewers to prioritize UV in all
    cases, and fallback to PDFjs as much as possible, regardless of
    feature flipper setting.
    
    Refs
    - https://github.com/samvera/hyku/pull/2336
    - https://github.com/scientist-softserv/adventist_knapsack/issues/817
    
    - Clean up views related to media viewers
    - Better documentation of overrides
    - Remove duplicated file set indexer decorator

--- app/presenters/hyku/work_show_presenter_decorator.rb
+++ app/presenters/hyku/work_show_presenter_decorator.rb
@@ -1,26 +1,39 @@
 # frozen_string_literal: true
+# Override Hyku 5.0.1
 
 module Hyku
   module WorkShowPresenterDecorator
-    def video_embed_viewer?
-      solr_document[:video_embed_tesim].present?
-    end
-
-    def video_embed_viewer
-      :video_embed_viewer
-    end
-
+    # Supports varying label on show page attributes by class
     def part_of_label
       klass = model_klass
       klass == JournalArticle ? 'Periodical' : 'Part Of'
     end
 
-    def pdf_viewer?
-      return unless Flipflop.default_pdf_viewer?
+    # OVERRIDE incorporates fallback to PDF.js viewer via fileset's import_url
+    # see also pdf_js_helper_decorator
+    def show_pdf_viewer?
+      return unless Flipflop.default_pdf_viewer? || !iiif_viewer?
       return unless file_set_presenters.any?(&:pdf?) || pdf_extension?
 
-      # If all of the member_presenters are file_set presenters, return true
-      # this also means that there are no child works
+      no_child_works?
+    end
+
+    # OVERRIDE Hyku TenantConfig
+    # @return [Boolean] render a IIIF viewer
+    def iiif_viewer?
+      no_children = no_child_works?
+      # Fall back to PDFjs viewer if we have a pdf that hasn't been split
+      return false if no_children && (file_set_presenters.any?(&:pdf?) || pdf_extension?)
+      # Fallback to UV even if PDFjs flag is on, as long as PDF has been split
+      Hyrax.config.iiif_image_server? &&
+        representative_id.present? &&
+        representative_presenter.present? &&
+        members_include_iiif_viewable? && 
+        (iiif_media? || !no_children)
+      end
+
+    # Returns true if all of the member_presenters are file_set presenters
+    def no_child_works?
       member_presenters.all? { |presenter| presenter.is_a? Hyrax::FileSetPresenter }
     end
 
@@ -28,8 +41,15 @@ module Hyku
       file_set_presenters.any? { |fsp| fsp&.label&.downcase&.end_with?('.pdf') }
     end
 
-    def viewer?
-      iiif_viewer? || video_embed_viewer? || pdf_viewer?
+    def iiif_media?(presenter: representative_presenter)
+      iiif_media_predicates.any? { |predicate| presenter.try(predicate) || presenter.try(:solr_document).try(predicate) }
+    end
+
+    # Override Tenant Config
+    def members_include_iiif_viewable?
+      iiif_presentable_member_presenters.any? do |presenter|
+        iiif_media?(presenter:) && current_ability.can?(:read, presenter.id)
+      end
     end
 
     private
