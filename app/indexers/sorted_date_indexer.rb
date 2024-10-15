925b087799d0016633eed066ea66fc931686e451
Shana Moore <shana@scientist.com>
2024-10-15 14:29:25 -0700

    :recycle: refactor
    addresses PR feedback

--- app/indexers/sorted_date_indexer.rb
+++ app/indexers/sorted_date_indexer.rb
@@ -4,17 +4,15 @@
 # A mixin for Valkyrie resources to support Date Range facet.
 module SortedDateIndexer
   # rubocop:disable Metrics/BlockLength
-  [:to_solr].each do |method_name|
-    define_method method_name do |*args, **kwargs, &block|
-      super(*args, **kwargs, &block).tap do |index_document|
-        if resource.date_created.present?
-          formatted_date = format_date(resource.date_created)
+  def to_solr
+    super.tap do |index_document|
+      if resource.date_created.present?
+        formatted_date = format_date(resource.date_created)
 
-          if formatted_date.present?
-            index_document['sorted_date_isi'] = format_full_date(formatted_date)
-            index_document['sorted_month_isi'] = format_year_month(formatted_date)
-            index_document['sorted_year_isi'] = format_year(formatted_date)
-          end
+        if formatted_date.present?
+          index_document['sorted_date_isi'] = format_full_date(formatted_date)
+          index_document['sorted_month_isi'] = format_year_month(formatted_date)
+          index_document['sorted_year_isi'] = format_year(formatted_date)
         end
       end
     end
@@ -23,9 +21,11 @@ module SortedDateIndexer
 
   private
 
-  # 'yyyy-mm-dd' format
+  # converts dates to format: 'yyyy-mm-dd'
   def format_date(date_created)
     date_created = date_created.is_a?(Array) ? date_created.first : date_created
+
+    return "#{date_created}-01-01" if date_created.match?(/\A\d{4}\z/)
 
     return date_created if date_created.match?(/\A\d{4}-\d{2}-\d{2}\z/)
 
